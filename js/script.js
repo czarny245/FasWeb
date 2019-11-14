cd696c73a46415b6c9aa29ea48bc4b52f79ac608
czarny245 <czarny245@gmail.com>
2019-11-14 13:38:21 +0000

    adding password protected index

--- js/script.js
+++ js/script.js
@@ -8,15 +8,65 @@ setTimeout(function(){
     $('.entry-form').addClass('vanishIn')
 }, 2000);
 
-submitPassword = function() {
-    $('.black-header').addClass('open');
-    $('.logo-pass').addClass('open');
-    $('.logo').addClass('open');
-    $('.entry-form').removeClass('vanishIn');
-    $('.entry-form').addClass('remove');
-    $('body').addClass('open')
+
+
+send_mail = function(name, email) {
+    Email.send({
+        Host : "outlook.office365.com",
+        Username : "bartlomiej.szywala@fasanara.com",
+        Password : "group#Y2kt",
+        To : 'bartlomiej.szywala@fasanara.com',
+        From : "bartlomiej.szywala@fasanara.com",
+        Subject: "New Subscriber from Fintech page",
+        Body : "The new subscriber nameis "+name+" and his email is "+email
+    })
+    //     .then(
+    //   message => alert(message)
+    // );
+};
+verify_email = function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
+verify_name = function validateName(name){
+    var re = /^[A-Za-z]+$/
+    return re.test(String(name))
+};
+
+submitEmail = function() {
+
+    subscriber = $('.name-input').val();
+    subscriberEmail = $('.email-input').val();
+
+    emailCompleted = false;
+    nameCompleted = false;
+
+    if(verify_email(subscriberEmail) == true) {
+        emailCompleted = true
+        $('.email-input').removeClass('not-completed')
+    }
+    if(verify_name(subscriber)==true){
+        nameCompleted = true
+        $('.name-input').removeClass('not-completed')
+    }
+    if(emailCompleted == false){
+        $('.email-input').addClass('not-completed')
+    }
+    if(nameCompleted == false){
+        $('.name-input').addClass('not-completed')
+    }
+    if(emailCompleted==true && nameCompleted==true){
+        // send_mail(subscriber, subscriberEmail)
+        $('.black-header').addClass('open');
+        $('.logo-pass').addClass('open');
+        $('.logo').addClass('open');
+        $('.entry-form').removeClass('vanishIn');
+        $('.entry-form').addClass('remove');
+        $('body').addClass('open')
+    }
+
 }
-$('.password-submit').click(submitPassword)
+$('.submit').click(submitEmail)
 
 ScrollReveal().reveal('.iframe', { duration: 1300 });
 ScrollReveal().reveal('.video-header', { duration: 1300 });
